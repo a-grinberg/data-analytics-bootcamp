c40990a7dcafe1a123ddd9522655e2de61158a5a
Fincher17 <xsonic17x@gmail.com>
2024-04-09 12:42:11 +0300

    Delete W1D6 directory

--- W1D6/Timed_Challenge_1.py
+++
@@ -1,5 +0,0 @@
-# Reverse The Sentence
-
-text = list(input("Enter a sentence: ").split(' '))
-text.sort(reverse=True)
-print(' '.join(text))
--- W1D6/Timed_Challenge_2.py
+++
@@ -1,8 +0,0 @@
-x = int(input('Enter the Number:')) 
-
-dividers = []
-for number in range(1,x):
-    if x % number == 0:
-        dividers.append(number)
-
-print(True if sum(dividers) == x else False)
