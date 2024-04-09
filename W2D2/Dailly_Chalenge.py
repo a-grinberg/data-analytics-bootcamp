ad77458abea52a37acbb2db036f81740ca506fcb
Fincher17 <xsonic17x@gmail.com>
2024-04-09 12:42:34 +0300

    Delete W2D2 directory

--- W2D2/Dailly_Chalenge.py
+++
@@ -1,6 +0,0 @@
-# Daily Challenge: Solve The Matrix
-
-string = '7iiTsxh%?i #sM $a #t%^r!'
-
-message = [[string[i+n] if string[i+n].isalpha() else ' ' for i in range(0, len(string), 3)] for n in range(3)]
-print(''.join(''.join(m) for m in message))
