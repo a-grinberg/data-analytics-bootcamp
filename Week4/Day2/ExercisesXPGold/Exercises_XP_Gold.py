d38c0705c8a6bdde9c0f3d49603101137127a8b6
Artem Grinberg <xsonic17x@gmail.com>
2024-05-12 11:58:47 +0300

    W4D5 - MiniProject weather app

--- Week4/Day2/ExercisesXPGold/Exercises_XP_Gold.py
+++ Week4/Day2/ExercisesXPGold/Exercises_XP_Gold.py
@@ -55,3 +55,33 @@ def validate_name(name):
 
 name = input('Enter your name: ')
 print(validate_name(name))
+
+# Exercise 5: Python Password Generator
+# Instructions
+# Create a Python program that will generate a good password for you.
+
+# Program flow:
+
+# Ask the user to type in the number of characters that the password should have (password length) – between 6 and 30 characters.
+# Validate the input. Make sure the user is inputing a number between 6 to 30. Create a loop which will continue to ask the user for an input until they enter a valid one.
+
+# Generate a password with the required length.
+
+# Print the password with a user-friendly message which reminds the user to keep the password in a safe place!
+
+# Rules for the validity of the password
+
+# Each password should contain:
+# At least 1 digit (0-9)
+# At least 1 lower-case character (a-z)
+# At least 1 upper-case character (A-Z)
+# At least 1 special character (eg. !, @, #, $, %, ^, _, …)
+# Once there is at least 1 of each, the rest of the password should be composed of more characters from the options presented above.
+
+# Create a test function first!
+
+# Do the following steps 100 times, with different password lengths:
+# Generate a password.
+# Test the password to ensure that:
+# it fulfills all the requirements above (eg. it has at least one digit, etc.)
+# it has the specified length.
--- Week4/Day5/MiniProject/Mini_Project_Weather_App.py
+++ Week4/Day5/MiniProject/Mini_Project_Weather_App.py
@@ -1,18 +1,27 @@
 from pyowm.owm import OWM
+import matplotlib
+import tkinter
+def get_forecast(city):
+    owm = OWM('ff2723c8cd446d052df25607ff1c159f')
+    mgr = owm.weather_manager()
+    reg = owm.city_id_registry()
+    city = reg.ids_for(city, matching='exact')[0]
+    city_id, city_name, country = city[0], city[1], city[2]
+    observation = mgr.weather_at_place(f"{city_name}, {country}")
+    weather = observation.weather
+    wind = observation.weather.wind()
+    sunrise_time = weather.sunrise_time('iso')
+    sunset_time = weather.sunset_time('iso')
+    
+    print(f"Current weather in {city_name}: {weather.status}, {weather.temperature('celsius')['temp']}°C")
+    print(f"Wind speed: {wind['speed']} m/s")
+    print(f"Sunrise time: {sunrise_time}")
+    print(f"Sunset time: {sunset_time}")
 
-owm = OWM('ff2723c8cd446d052df25607ff1c159f')
-mgr = owm.weather_manager()
-reg = owm.city_id_registry()
-print(reg)
-observation = mgr.weather_at_place("Tel-Aviv, IL")
-weather = observation.weather
-wind = observation.weather.wind()
-sunrise_time = weather.sunrise_time('iso')
-sunset_time = weather.sunset_time('iso')
+    three_h_forecast = mgr.forecast_at_id(city_id, '3h')
+    for weather in three_h_forecast.forecast:
+        print(weather.reference_time('iso'), weather.detailed_status)
+    
 
-
-print(f"Current weather in Tel Aviv: {weather.status}, {weather.temperature('celsius')['temp']}°C")
-print(f"Wind speed: {wind['speed']} m/s")
-print(f"Sunrise time: {sunrise_time}")
-print(f"Sunset time: {sunset_time}")
-
+city = input('Please enter your city: ')
+get_forecast(city)
